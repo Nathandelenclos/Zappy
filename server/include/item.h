a452c5dd3fa6a007c29c07b683d5698c0353701a
Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-06-09 19:25:33 +0200

    [+] Placed items randomly on the map according to their density

--- server/include/item.h
+++ server/include/item.h
@@ -23,7 +23,23 @@ typedef enum {
 } item_type_t;
 
 typedef struct {
-    item_type_t name;
+    item_type_t type;
+    double density;
+} type_density_t;
+
+typedef struct {
+    item_type_t type;
 } item_t;
+
+static type_density_t type_density[] = {
+    {FOOD, 0.5},
+    {LINEMATE, 0.3},
+    {DERAUMERE, 0.15},
+    {SIBUR, 0.1},
+    {MENDIANE, 0.1},
+    {PHIRAS, 0.08},
+    {THYSTAME, 0.05},
+    {PLAYER, 0.0}
+};
 
 #endif /* !ITEM_H_ */
